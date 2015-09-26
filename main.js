e1f4746e50e7903bf30ea4247d580b0255a566c8
Fred Cox <mcfedr@gmail.com>
2015-09-26 21:45:32 +0300

    Add some command line ops and build the files on install

--- main.js
+++ main.js
@@ -2,7 +2,18 @@
 var express = require("express"),
     morgan  = require('morgan'),
     sender = require('./server/sender'),
-    config = require('./server/config');
+    config = require('./server/config'),
+    commander = require('commander');
+
+var options = commander
+    .description('Send local videos to your chromecast')
+    .option('-t, --tv <path>', 'Folder with videos')
+    .option('-p, --port <port>', 'Server port')
+    .parse(process.argv)
+    .opts();
+
+config.port = options.port || config.port;
+config.videosDir = options.tv || config.videosDir;
 
     app = express()
         .use(morgan('dev'))
